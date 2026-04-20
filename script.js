209de83f0edf24954eb653326179aeac9e4a1e8e
Cristofer Ballesteros <cristofer-531@hotmail.com>
2026-04-19 19:52:25 -0600

    Primer commit  integrando estilos al footer y funciones js

---
+++ script.js
@@ -0,0 +1,37 @@
+//Función para mover el carousel con los botones
+function moveSlide(trackId, dotsId, direction) {
+    const track = document.getElementById(trackId);
+
+    //Verificar si el track existe antes de intentar manipularlo
+    if (!track) return;
+
+    //Busca todas la imagenes dentro del carousel
+    const slides = track.querySelectorAll('.carousel-slide'); 
+    //Busca el contenedor de los puntos de navegación
+    const dotsContainer = document.getElementById(dotsId);
+    //Busca los puntos dentro del contenedor
+    const dots = dotsContainer.querySelectorAll('.dot');
+
+    //Encuentra el índice de la imagen activa actualmente
+    let activeIndex = Array.from(slides).findIndex(slide => slide.classList.contains('active'));
+
+    //Remueve la clase 'active' de la imagen y el punto actual para ocultarlos
+    slides[activeIndex].classList.remove('active');
+    dots[activeIndex].classList.remove('active');
+
+    let newIndex = activeIndex + direction;
+    //Si el nuevo índice es menor que 0, vuelve al último slide
+    if (newIndex >= slides.length) newIndex = 0;
+    //Si el nuevo índice es mayor o igual al número de slides, vuelve al primer slide
+    if (newIndex < 0) newIndex = slides.length - 1;
+
+    //Agrega la clase 'active' a la nueva imagen y al nuevo punto para mostrarlos
+    slides[newIndex].classList.add('active');
+    dots[newIndex].classList.add('active');
+
+}
+
+    setInterval(() => {
+        moveSlide('track1', 'dots1', 1);
+        moveSlide('track2', 'dots2', 1); 
+    }, 3000);
